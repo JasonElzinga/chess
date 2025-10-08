6eb8e60856acd6a54ddc83839bb4c4e5accf91d4
JasonElzinga <jason.elzinga27@gmail.com>
2025-10-08 15:09:32 -0600

    added the register

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,11 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+import io.javalin.json.JavalinGson;
+
+import java.util.Map;
 
 public class Server {
 
@@ -9,9 +14,21 @@ public class Server {
     public Server() {
         server = Javalin.create(config -> config.staticFiles.add("web"));
         server.delete("db", ctx -> ctx.result("{}"));
-        server.post("user", ctx -> ctx.result("{\"username\": \"joe\", \"password\": \"123\", \"email\": \"joe.email\"}"));
+
+        server.post("user", this::register);
         // Register your endpoints and exception handlers here.
 
+    }
+
+
+    private void register(Context cxt) {
+        var serializer = new Gson();
+        var req = serializer.fromJson(cxt.body(), Map.class);
+        req.put("authToken", "cow");
+        //var user = req.username;
+        var res = serializer.toJson(req);
+
+        cxt.result(res);
     }
 
     public int run(int desiredPort) {
