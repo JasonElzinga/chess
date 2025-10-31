3788b07f6b8c1fea0d3341d30b30872bf9befe41
JasonElzinga <jason.elzinga27@gmail.com>
2025-10-30 21:52:47 -0600

    added mySqlDataAccess

---
+++ server/src/main/java/dataaccess/mySqlDataAccess.java
@@ -0,0 +1,93 @@
+package dataaccess;
+
+import model.AuthData;
+import model.CreateGameResponse;
+import model.GameData;
+import model.UserData;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class mySqlDataAccess implements DataAccess{
+
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public UserData getUser(String username) {
+        return null;
+    }
+
+    @Override
+    public void createUser(UserData user) {
+
+    }
+
+    @Override
+    public void storeAuth(AuthData auth) {
+
+    }
+
+    @Override
+    public AuthData getAuthData(String authToken) {
+        return null;
+    }
+
+    @Override
+    public void deleteAuthData(AuthData auth) {
+
+    }
+
+    @Override
+    public CreateGameResponse createGame(String gameName) {
+        return null;
+    }
+
+    @Override
+    public List<GameData> listGames() {
+        return List.of();
+    }
+
+    @Override
+    public void joinGame(String username, String playerColor, Integer gameID) {
+
+    }
+
+    @Override
+    public GameData getGame(Integer gameID) {
+        return null;
+    }
+
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS  chess (
+              `id` int NOT NULL AUTO_INCREMENT,
+              `name` varchar(256) NOT NULL,
+              `type` ENUM('CAT', 'DOG', 'FISH', 'FROG', 'ROCK') DEFAULT 'CAT',
+              `json` TEXT DEFAULT NULL,
+              PRIMARY KEY (`id`),
+              INDEX(type),
+              INDEX(name)
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            """
+    };
+
+
+    private void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            for (String statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", e.getMessage()));
+        }
+    }
+
+}
