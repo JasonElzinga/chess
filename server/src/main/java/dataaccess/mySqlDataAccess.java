3bbf0ff11b66276a794453899378c3479796d3cd
JasonElzinga <jason.elzinga27@gmail.com>
2025-10-31 12:36:52 -0600

    adupdated the createUser

--- server/src/main/java/dataaccess/mySqlDataAccess.java
+++ server/src/main/java/dataaccess/mySqlDataAccess.java
@@ -1,13 +1,20 @@
 package dataaccess;
 
+import chess.ChessGame;
+import com.google.gson.Gson;
 import model.AuthData;
 import model.CreateGameResponse;
 import model.GameData;
 import model.UserData;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class mySqlDataAccess implements DataAccess{
 
@@ -22,21 +29,15 @@ public class mySqlDataAccess implements DataAccess{
 
     @Override
     public UserData getUser(String username) {
-        try (Connection conn = DatabaseManager.getConnection()) {
-            for (String statement : createStatements) {
-                try (var preparedStatement = conn.prepareStatement(statement)) {
-                    preparedStatement.executeUpdate();
-                }
-            }
-        } catch (SQLException e) {
-            //throw new DataAccessException(String.format("Unable to configure database: %s", e.getMessage()));
-        }
-        return null;
+
     }
 
     @Override
     public void createUser(UserData user) {
-
+        var statement = "INSERT INTO pet (name, type, json) VALUES (?, ?, ?)";
+        String json = new Gson().toJson(pet);
+        int id = executeUpdate(statement, pet.name(), pet.type(), json);
+        return new Pet(id, pet.name(), pet.type());
     }
 
     @Override
@@ -72,6 +73,33 @@ public class mySqlDataAccess implements DataAccess{
     @Override
     public GameData getGame(Integer gameID) {
         return null;
+    }
+
+
+    private int executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+                    if (param instanceof String p) ps.setString(i + 1, p);
+                    else if (param instanceof Integer p) ps.setInt(i + 1, p);
+                    //else if (param instanceof ChessGame p) ps.setString(i + 1, serializer.toJson(p));
+                    else if (param == null) ps.setNull(i + 1, NULL);
+                }
+                ps.executeUpdate();
+
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException(String.format("unable to update database: %s, %s", statement, e.getMessage()));
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private final String[] createStatements = {
