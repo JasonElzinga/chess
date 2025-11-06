66e893f184c2408290d765fba505d8bfb15ac9a1
JasonElzinga <jason.elzinga27@gmail.com>
2025-11-06 13:33:15 -0700

    added in printing out the black and white rook

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -10,12 +10,14 @@ public class Main {
         ChessGame board = new ChessGame();
         System.out.println("♕ 240 Chess Client: type help to get started.");
         Scanner scanner = new Scanner(System.in);
+        var main = new Main();
         while (true) {
             var line = scanner.nextLine();
-            System.out.println(EscapeSequences.SET_TEXT_COLOR_BLACK);
-            System.out.println(line + EscapeSequences.BLACK_BISHOP);
-            System.out.println((EscapeSequences.SET_BG_COLOR_DARK_GREEN));
-            System.out.println(line + EscapeSequences.WHITE_BISHOP);
+//            System.out.println(EscapeSequences.SET_TEXT_COLOR_BLACK);
+//            System.out.println(line + EscapeSequences.BLACK_BISHOP);
+//            System.out.println((EscapeSequences.SET_BG_COLOR_DARK_GREEN));
+//            System.out.println(line + EscapeSequences.WHITE_BISHOP);
+            main.drawChessBoard(board, ChessGame.TeamColor.WHITE);
         }
     }
 
@@ -59,11 +61,27 @@ public class Main {
 //                EscapeSequences.WHITE_ROOK,
 //        };
 
-        for (int row = 0; row <= 9; row++) {
-            if (row == 0 || row == 9) {
-                System.out.println(EscapeSequences.SET_TEXT_COLOR_DARK_GREY);
-                for (int col = 1; col <=9; col++) {
+        var currentBoard = board.getBoard();
+        boolean whitePerspective = (color == ChessGame.TeamColor.WHITE);
 
+        int rowStart = whitePerspective ? 1 : 8;
+        int rowStep = whitePerspective ? 1 : -1;
+        int rowEnd = whitePerspective ? 8 : 1;
+
+
+        for (int row = rowStart; row != rowEnd; row +=rowStep) {
+            System.out.println(EscapeSequences.SET_TEXT_COLOR_DARK_GREY);
+            for (int col = rowStart; col != rowEnd; col +=rowStep) {
+                var piece = currentBoard.getPiece(new ChessPosition(row, col));
+                if (piece == null) {
+                    //
+                }
+                else {
+                    switch (piece.getPieceType()) {
+                        case ROOK -> System.out.println(
+                                piece.getTeamColor() == ChessGame.TeamColor.WHITE ?
+                                        EscapeSequences.WHITE_ROOK : EscapeSequences.BLACK_ROOK);
+                    }
                 }
             }
         }
